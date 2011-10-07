79622520cb6481eb87526a7506963160367ef14f
Eric Schoonover <erics@capdes.com>
2011-10-07 16:50:22 -0700

    working locally

--- web.py
+++ web.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello from Python!"
+  return "Hello World!"
 
-if __name__ == "__main++":
+if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
   app.run(host='0.0.0.0', port=port)
