b64374006b0f26832222bf2159174855f839645d
Eric Schoonover <erics@capdes.com>
2011-10-11 01:37:57 -0700

    fixed published to created to match proper field name in entry rendering on home page

--- dblog/blog/views.py
+++ dblog/blog/views.py
@@ -8,7 +8,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 def home(request):
-  entries = Entry.objects.filter(state='P').order_by('-published')
+  entries = Entry.objects.filter(state='P').order_by('-created')
   p = Paginator(entries, 2)
 
   try:
